288b8049acb92074813be9de0daf393aa6596305
rosthts <rosthts@gmail.com>
2020-08-10 18:09:14 +0300

    First

---
+++ app/src/main/java/com/example/android/thefirst/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.thefirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickNewGame(View view){
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(intent);
+    }
+}
